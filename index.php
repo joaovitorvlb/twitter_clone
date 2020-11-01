bbe64e67c52fde819150e5e0af589bb17a259251
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-01 10:51:40 -0300

    validando formulario de login

--- index.php
+++ index.php
@@ -21,9 +21,23 @@
 
 				$('#btn_login').click(function(){
 
+					var campo_vazio = false;
+
 					if($('#campo_usuario').val() == ''){
 						$('#campo_usuario').css({'border-color' : '#A94442'});
+						campo_vazio = true;
+					}else{
+						$('#campo_usuario').css({'border-color' : '#CCC'});
 					}
+
+					if($('#campo_senha').val() == ''){
+						$('#campo_senha').css({'border-color' : '#A94442'});
+						campo_vazio = true;
+					}else{
+						$('#campo_senha').css({'border-color' : '#CCC'});
+					}
+
+					if(campo_vazio) return false;
 
 				});
 			});
