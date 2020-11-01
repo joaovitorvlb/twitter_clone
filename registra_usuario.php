4e1f412d32d520dc48301d02cab49045d8669aab
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-01 18:36:31 -0300

    Adicionado registro de usuario

--- registra_usuario.php
+++ registra_usuario.php
@@ -1,9 +1,22 @@
 <?php
+	require_once('db.class.php');
 
-    echo $_POST['usuario'];
-    echo "<br />";
-    echo $_POST['email'];
-    echo "<br />";
-    echo $_POST['senha'];
+    $usuario = $_POST['usuario'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
+    $objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+    $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
+
+    //executar a query
+    if(mysqli_query($link, $sql)){
+        echo "Usuário registrado com sucesso!";
+    }else{
+        echo "Erro ao registrar o usuário!";
+    }
+
+	  
+    printf("Errormessage: %s\n", mysqli_error($link));
 ?>
