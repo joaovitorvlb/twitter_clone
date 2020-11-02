5b1826c9ad82898acc062ecc801dd0e035320cb4
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-02 11:46:01 -0300

    verificar se o e-mail já existe

--- registra_usuario.php
+++ registra_usuario.php
@@ -28,6 +28,29 @@
         echo 'Erro ao tentar localizar o registro de usuário';
     }
 
+    //verificar se o e-mail já existe
+    $sql = "SELECT * FROM usuarios WHERE email = '$email' ";
+    if($resultado_id = mysqli_query($link, $sql)){
+
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+
+        if(isset($dados_usuario['email'])){
+
+            echo 'Email já cadastrado';
+
+        }else{
+
+            echo 'Email não cadastrado, ok, pode cadastrar';
+
+        }
+
+    }else{
+        echo 'Erro ao tentar localizar o registro de email';
+    }
+
+    die();
+
+
     $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
 
     //executar a query
