ea46a5da4f2fa0947e5665977972fd4829825a7e
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-02 10:36:11 -0300

    selecionar domente usuario e email

--- validar_acesso.php
+++ validar_acesso.php
@@ -1,11 +1,13 @@
 <?php
 
+    session_start();
+
     require_once('db.class.php');
 
     $usuario = $_POST['usuario'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
+    $sql = "SELECT usuario, email FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
 
     $objDb = new db();
     $link = $objDb->conecta_mysql();
@@ -15,22 +17,26 @@
     if($resultado_id){
 
         $dados_usuario = mysqli_fetch_array($resultado_id);
+
         if(isset($dados_usuario['usuario'])){
 
-            echo 'usuário existe';
+            $_SESSION['usuario'] = $dados_usuario['usuario'];
+            $_SESSION['email'] = $dados_usuario['email'];
+
+            echo $_SESSION['usuario'];
+            echo $_SESSION['email'];
+
+            header('Location: test.php');
 
         }else{
 
             header('Location: index.php?erro=1');
-
         }
 
     }else{
         
         echo "Erro na execução da consulta, favor entrar em contato com o admin do site";
-
     }
-
 ?>
 
 
