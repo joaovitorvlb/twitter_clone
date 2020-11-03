430b587787a567a5525f273c57ecd16c5223b19e
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-02 23:11:11 -0300

    adicionar link na lista para cada pessoa

--- get_pessoas.php
+++ get_pessoas.php
@@ -23,11 +23,12 @@
         while($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)){
 
             echo '<a href="#" class="list-group-item">';
-
-                echo '<strong>' . $registro['usuario'] . '</strong> <small> - ' . $registro['email'] . ' </small>';
-
+            echo '<strong>' . $registro['usuario'] . '</strong> <small> - ' . $registro['email'] . ' </small>';
+            echo '<p class="list-group-item-text pull-right">';
+            echo '<button type="button" class="btn btn-default">Seguir</button>';
+            echo '</p>';
+            echo '<div class="clearfix"></div>';
             echo '</a>';
-
         }
 
     }else{
