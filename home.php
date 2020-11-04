7068c59e4557e19bccb7664fec0d059c9687bcab
Joao Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2020-11-04 13:16:12 -0300

    Selecionando aquantidade de tweets do usuario

--- home.php
+++ home.php
@@ -7,6 +7,35 @@ if((!isset ($_SESSION['usuario']) == true) and (!isset ($_SESSION['senha']) == t
 	unset($_SESSION['senha']);
 	header('Location: index.php?erro=1');
 }
+
+	require_once('db.class.php');
+
+	$objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+	$id_usuario = $_SESSION['id_usuario'];
+
+	//--qtde de tweets
+	$sql = "SELECT COUNT(*) AS qtde_tweets FROM tweet WHERE id_usuario = $id_usuario";
+	$resultado_id = mysqli_query($link, $sql);
+	$qtde_tweets = 0;
+	if($resultado_id){
+		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+		$qtde_tweets = $registro['qtde_tweets'];
+	}else{
+		echo 'Erro ao executar a query';
+	}
+
+	//--qtde de seguidores
+	$sql = "SELECT COUNT(*) AS qtde_seguidores FROM usuarios_seguidores WHERE seguindo_id_usuario = $id_usuario";
+	$resultado_id = mysqli_query($link, $sql);
+	$qtde_seguidores = 0;
+	if($resultado_id){
+		$registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC);
+		$qtde_seguidores = $registro['qtde_seguidores'];
+	}else{
+		echo 'Erro ao executar a query';
+	}
 ?>
 
 <!DOCTYPE HTML>
